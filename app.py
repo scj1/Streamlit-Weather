2fd8b673645e609934284b4a6a01355d92075b7c
scj1 <94457724+scj1@users.noreply.github.com>
2022-02-02 18:05:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 import streamlit as st
+import requests
+
+url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+querystring = {"q":"London,uk","lat":"0","lon":"0","callback":"test","id":"2172797","lang":"null","units":"imperial","mode":"xml"}
+
+headers = {
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+    'x-rapidapi-key': "4a5237600amsh2d283547f53b90ap12f53djsn09b9149d542d"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
+
+
+
+
 st.title("Streamlit Weather")
