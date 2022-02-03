1277c42cf532e4a883e482d03ea8edb6347b96d2
scj1 <94457724+scj1@users.noreply.github.com>
2022-02-03 19:43:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,18 +18,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 
-url = "https://community-open-weather-map.p.rapidapi.com/weather"
 
-querystring = {"q":"London,uk","lat":"0","lon":"0","callback":"test","id":"2172797","lang":"null","units":"imperial","mode":"xml"}
-
-headers = {
-    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
-    'x-rapidapi-key': "4a5237600amsh2d283547f53b90ap12f53djsn09b9149d542d"
-    }
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-q = ['Sydney', 'Colombo', 'Los Angeles', 'London', 'Rome', 'Paris']
 
 st.sidebar.selectbox('Pick a country', q )
 
